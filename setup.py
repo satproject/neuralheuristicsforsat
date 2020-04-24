52fd7817cef73e9f54e9a749514a9cbd4a7e5d66
Marcin Wierzbiński <marcin119a@gmail.com>
2020-04-24 12:41:02 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+import io
+
+from setuptools import find_packages, setup
+
+with io.open('README.rst', 'rt', encoding='utf8') as f:
+    readme = f.read()
+
+setup(
+    name='projekt_bazy',
+    version='1.0.0',
+    url='https://github.com/satproject/neuralheuristicsforsat/',
+    license='MIT',
+    maintainer='SAT TEAM',
+    long_description=readme,
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'jupyterlab',
+        'tensorflow==1.12',
+        'sympy',
+        'sklearn',
+        'scipy',
+        'matplotlib'
+        'pandas',
+        'python-sat',
+        'google-api-python-client',
+        'google-cloud-storage',
+        'oauth2client',
+        'tqdm'
+    ],
+)
